cdf25716905d92217f1e1c1428113ef10665ef48
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-07-05 17:43:36 +0200

    Remove empty-program check in executor
    * supported by FPGA, thus user fault, not checked on this layer
    
    Change-Id: Icf8787ce4c7acd3feb3dc1828f1a8e219add187e

--- src/fisch/vx/playback_executor.cpp
+++ src/fisch/vx/playback_executor.cpp
@@ -5,10 +5,6 @@ namespace fisch::vx {
 template <class Connection>
 void PlaybackProgramExecutor<Connection>::run(std::shared_ptr<PlaybackProgram> const& program)
 {
-	if (program->get_to_fpga_messages().empty()) {
-		throw std::logic_error("Trying to run empty PlaybackProgram.");
-	}
-
 	m_connection.add(program->get_to_fpga_messages());
 	m_connection.add(hxcomm::vx::UTMessageToFPGA<hxcomm::vx::instruction::system::Halt>());
 	m_connection.commit();
@@ -27,10 +23,6 @@ template <class Connection>
 std::vector<PlaybackProgram::from_fpga_message_type> PlaybackProgramExecutor<Connection>::run(
     std::vector<PlaybackProgram::to_fpga_message_type> const& messages)
 {
-	if (messages.empty()) {
-		throw std::logic_error("Trying to run empty UT message sequence.");
-	}
-
 	m_connection.add(messages);
 	m_connection.add(hxcomm::vx::UTMessageToFPGA<hxcomm::vx::instruction::system::Halt>());
 	m_connection.commit();
