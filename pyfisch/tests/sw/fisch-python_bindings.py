7955fbd8f32d18deb20a46041bb7309a6a3a517e
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-08-21 22:52:15 +0200

    Fix python test
    Change-Id: I6bbbd7a48d32805f6fb8bf6b283330fc3e7a7fa0

--- pyfisch/tests/sw/fisch-python_bindings.py
+++ pyfisch/tests/sw/fisch-python_bindings.py
@@ -14,7 +14,7 @@ def get_all_classes(m):
     ret = dict()
     for name, obj in inspect.getmembers(m):
         if (inspect.isclass(obj) and
-                str(obj.__class__) == "<class 'pybind11_type'>"):
+                str(obj.__class__) == "<class 'pybind11_builtins.pybind11_type'>"):
             ret[name] = obj
             get_all_classes(obj)
     return ret
