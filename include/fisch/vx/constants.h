397a3540f37b59d45bbd53a7196e2a8a9f0971d0
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-12-11 23:52:34 +0100

    Remove hxcomm include from constants.h
    Change-Id: Idf981a0f97fcfa786cdbbdd12ba34b25ed9c7b82

--- include/fisch/vx/constants.h
+++ include/fisch/vx/constants.h
@@ -1,15 +1,12 @@
 #pragma once
-#include "hxcomm/vx/instruction/timing.h"
-
+#include <stddef.h>
 #include "fisch/vx/genpybind.h"
 
 namespace fisch {
 namespace vx GENPYBIND_TAG_FISCH_VX {
 
-typedef hxcomm::vx::instruction::timing::WaitUntil::value_type hardware_timer_value_type;
-
 /** Number of FPGA clock cycles per microsecond. */
-constexpr hardware_timer_value_type GENPYBIND(visible) fpga_clock_cycles_per_us = 125;
+constexpr size_t GENPYBIND(visible) fpga_clock_cycles_per_us = 125;
 
 } // namespace vx
 } // namespace fisch
