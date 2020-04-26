8a0773cfcf74c1af9cf9f87480390633db81d4df
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2020-04-26 12:45:05 +0200

    Improve ContainerTicket::get() return value construction
    * random-access DecodeIterator used to use std::vector(begin, end)
      constructor, which allocates only once and in-place constructs
      elements (without previous default construction)
    * only one heap allocation -> O(1) (O(log(N)) previously)
    * only two default constructed containers (begin and end iterator)
      -> O(1) (O(1) previously)
    * no push_back() that has to check for free
      reserved space each call
    
    Change-Id: I73e2edb377893298f5d203207635fbc361ddb0d5

--- src/fisch/vx/container_ticket.cpp
+++ src/fisch/vx/container_ticket.cpp
@@ -6,6 +6,8 @@
 #include "hate/type_list.h"
 #include "hxcomm/vx/utmessage.h"
 
+#include <boost/iterator/iterator_facade.hpp>
+
 namespace fisch::vx {
 
 template <class ContainerT>
@@ -31,6 +33,70 @@ ContainerTicket<ContainerT>::~ContainerTicket()
 	m_pbp->deregister_ticket(this);
 }
 
+namespace detail {
+
+/**
+ * Random-access iterator for decoding a sequence of containers on the fly.
+ * @tparam ContainerT Type of container
+ * @tparam MessageIterator Random-access iterator for message sequence
+ */
+template <typename ContainerT, typename MessageIterator>
+struct DecodeIterator
+    : public boost::iterator_facade<
+          DecodeIterator<ContainerT, MessageIterator>,
+          ContainerT,
+          boost::random_access_traversal_tag>
+{
+	DecodeIterator(MessageIterator const begin) : it(begin), config() {}
+
+private:
+	MessageIterator it;
+	mutable ContainerT config;
+
+	friend class boost::iterator_core_access;
+
+	bool equal(DecodeIterator const& other) const
+	{
+		return it == other.it;
+	}
+
+	void decrement()
+	{
+		it -= ContainerT::decode_ut_message_count;
+	}
+
+	void increment()
+	{
+		it += ContainerT::decode_ut_message_count;
+	}
+
+	void advance(typename MessageIterator::difference_type const n)
+	{
+		it += n * ContainerT::decode_ut_message_count;
+	}
+
+	typename MessageIterator::difference_type distance_to(DecodeIterator const other) const
+	{
+		return std::distance(it, other.it) / ContainerT::decode_ut_message_count;
+	}
+
+	ContainerT& dereference() const
+	{
+		config.decode({it, it + ContainerT::decode_ut_message_count});
+		return config;
+	}
+
+	/**
+	 * Ensure it is really a random access iterator to make use of efficient std::vector(begin, end)
+	 * constructor.
+	 */
+	static_assert(std::is_base_of_v<
+	              std::random_access_iterator_tag,
+	              typename DecodeIterator::iterator_category>);
+};
+
+} // namespace detail
+
 template <class ContainerT>
 std::vector<ContainerT> ContainerTicket<ContainerT>::get() const
 {
@@ -41,17 +107,14 @@ std::vector<ContainerT> ContainerTicket<ContainerT>::get() const
 
 		std::vector<ContainerT> containers;
 
-		for (size_t c = 0; c < m_container_count; ++c) {
-			auto const& queue =
-			    std::get<detail::decode_message_types_index<ContainerT>>(m_pbp->m_receive_queue);
+		auto const& queue =
+		    std::get<detail::decode_message_types_index<ContainerT>>(m_pbp->m_receive_queue);
 
-			ContainerT config;
-			auto const begin_it =
-			    queue.get_messages().cbegin() + m_pos + c * ContainerT::decode_ut_message_count;
-			config.decode({begin_it, begin_it + ContainerT::decode_ut_message_count});
-			containers.push_back(config);
-		}
-		return containers;
+		auto const begin_it = queue.get_messages().cbegin() + m_pos;
+		typedef detail::DecodeIterator<ContainerT, std::remove_cv_t<decltype(begin_it)>> Iterator;
+		return std::vector<ContainerT>(
+		    Iterator(begin_it),
+		    Iterator(begin_it + ContainerT::decode_ut_message_count * m_container_count));
 	} else {
 		throw std::logic_error("Container not readable.");
 	}
