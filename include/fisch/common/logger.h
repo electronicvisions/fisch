acc46e10a31b89864de213df31969ccd05bdfb33
Eric Müller <mueller@kip.uni-heidelberg.de>
2020-09-04 10:31:38 +0200

    Log warnings and errors to syslog
    Change-Id: I557b038517d14a177460b5ed7700feaa9378fbaa

--- include/fisch/common/logger.h
+++ include/fisch/common/logger.h
@@ -1,6 +1,20 @@
 #pragma once
 #include <log4cxx/logger.h>
 
+extern "C"
+{
+#include <syslog.h>
+}
+
+#define FISCH_LOG_SYSLOG(level, message)                                                           \
+	{                                                                                              \
+		std::stringstream msg;                                                                     \
+		msg << message;                                                                            \
+		openlog(NULL, (LOG_NOWAIT | LOG_NDELAY | LOG_PID), LOG_USER);                              \
+		syslog(level, "(%s) %s", #level, msg.str().c_str());                                       \
+		closelog();                                                                                \
+	}
+
 #if !defined(FISCH_LOG_THRESHOLD) || FISCH_LOG_THRESHOLD <= 0
 #define FISCH_LOG_TRACE(logger, message) LOG4CXX_TRACE(logger, message)
 #else
@@ -20,19 +34,25 @@
 #endif
 
 #if !defined(FISCH_LOG_THRESHOLD) || FISCH_LOG_THRESHOLD <= 3
-#define FISCH_LOG_WARN(logger, message) LOG4CXX_WARN(logger, message)
+#define FISCH_LOG_WARN(logger, message)                                                            \
+	LOG4CXX_WARN(logger, message)                                                                  \
+	FISCH_LOG_SYSLOG(LOG_WARNING, message)
 #else
-#define FISCH_LOG_WARN(logger, message)
+#define FISCH_LOG_WARN(logger, message) FISCH_LOG_SYSLOG(LOG_WARNING, message)
 #endif
 
 #if !defined(FISCH_LOG_THRESHOLD) || FISCH_LOG_THRESHOLD <= 4
-#define FISCH_LOG_ERROR(logger, message) LOG4CXX_ERROR(logger, message)
+#define FISCH_LOG_ERROR(logger, message)                                                           \
+	LOG4CXX_ERROR(logger, message)                                                                 \
+	FISCH_LOG_SYSLOG(LOG_ERR, message)
 #else
-#define FISCH_LOG_ERROR(logger, message)
+#define FISCH_LOG_ERROR(logger, message) FISCH_LOG_SYSLOG(LOG_ERR, message)
 #endif
 
 #if !defined(FISCH_LOG_THRESHOLD) || FISCH_LOG_THRESHOLD <= 5
-#define FISCH_LOG_FATAL(logger, message) LOG4CXX_FATAL(logger, message)
+#define FISCH_LOG_FATAL(logger, message)                                                           \
+	LOG4CXX_FATAL(logger, message)                                                                 \
+	FISCH_LOG_SYSLOG(LOG_CRIT, message)
 #else
-#define FISCH_LOG_FATAL(logger, message)
+#define FISCH_LOG_FATAL(logger, message) FISCH_LOG_SYSLOG(LOG_CRIT, message)
 #endif
