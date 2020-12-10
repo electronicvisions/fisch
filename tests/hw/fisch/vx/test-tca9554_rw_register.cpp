618d8179e90b58786bb08a2210bd20a0f5671769
Falk Ebert <falk.ebert@kip.uni-heidelberg.de>
2020-12-10 13:28:36 +0100

    Add I2C registers for TCA9554 (io expander)
    Depends-On: 12649
    
    Change-Id: I0ec9fc4e0834434f7a2c9a3fc9ad1ecdfeb4d60d

---
+++ tests/hw/fisch/vx/test-tca9554_rw_register.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+#include "fisch/vx/barrier.h"
+#include "fisch/vx/container_ticket.h"
+#include "fisch/vx/i2c.h"
+#include "fisch/vx/omnibus_constants.h"
+#include "fisch/vx/playback_program.h"
+#include "fisch/vx/playback_program_builder.h"
+#include "fisch/vx/run.h"
+#include "halco/hicann-dls/vx/barrier.h"
+#include "halco/hicann-dls/vx/i2c.h"
+#include "hxcomm/vx/simconnection.h"
+
+#include "connection.h"
+
+using namespace halco::hicann_dls::vx;
+using namespace fisch::vx;
+
+// We currently do not have the chip accessible in normal setups
+TEST(I2CTCA9554RwRegister, DISABLED_Rw)
+{
+	PlaybackProgramBuilder builder;
+
+	I2CTCA9554RwRegister config;
+	config.set(I2CTCA9554RwRegister::Value(0xC0));
+	I2CTCA9554RwRegisterOnBoard coord(I2CTCA9554RwRegisterOnTCA9554::config, TCA9554OnBoard());
+
+	builder.write(i2c_prescaler_base_address, Omnibus(Omnibus::Value(313)));
+	builder.write(coord, config);
+	auto ticket = builder.read(coord);
+
+	builder.write(BarrierOnFPGA(), Barrier(Barrier::Value::omnibus));
+	auto program = builder.done();
+
+	auto connection = generate_test_connection();
+	if constexpr (std::is_same_v<decltype(connection), hxcomm::vx::SimConnection>) {
+		GTEST_SKIP() << "TCA9554 Register write read test only works in hardware.";
+	}
+	run(connection, program);
+
+	EXPECT_TRUE(ticket.valid());
+	EXPECT_NO_THROW(ticket.get());
+	EXPECT_EQ(ticket.get().at(0), config);
+}
---
+++ tests/sw/fisch/vx/test-i2c_tca9554_rw_register.cpp
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+
+#include "fisch/cerealization.h"
+#include "fisch/vx/i2c.h"
+#include "fisch/vx/omnibus_constants.h"
+#include "halco/hicann-dls/vx/i2c.h"
+#include "test-macros.h"
+
+using namespace fisch::vx;
+
+FISCH_TEST_NUMBER_REGISTER_GENERAL(I2CTCA9554RwRegister, 0, 0x55)
+FISCH_TEST_NUMBER_REGISTER_CEREAL(I2CTCA9554RwRegister, 0x55)
+
+TEST(I2CTCA9554RwRegister, EncodeRead)
+{
+	using namespace hxcomm::vx;
+
+	typename I2CTCA9554RwRegister::coordinate_type coord(
+	    halco::hicann_dls::vx::I2CTCA9554RwRegisterOnTCA9554::polarity,
+	    halco::hicann_dls::vx::TCA9554OnBoard());
+	auto messages = I2CTCA9554RwRegister::encode_read(coord);
+
+	EXPECT_EQ(messages.size(), 3);
+	auto addr_write = UTMessageToFPGA<instruction::omnibus_to_fpga::Address>(
+	    instruction::omnibus_to_fpga::Address::Payload(i2c_tca9554_base_address, false));
+
+	auto addr_read_stop = UTMessageToFPGA<instruction::omnibus_to_fpga::Address>(
+	    instruction::omnibus_to_fpga::Address::Payload(
+	        i2c_tca9554_base_address | i2c_over_omnibus_stop, true));
+
+	auto message_addr_1 =
+	    std::get<UTMessageToFPGA<instruction::omnibus_to_fpga::Address>>(messages.at(0));
+	EXPECT_EQ(message_addr_1, addr_write);
+	auto message_data_1 =
+	    std::get<UTMessageToFPGA<instruction::omnibus_to_fpga::Data>>(messages.at(1));
+	EXPECT_EQ(
+	    message_data_1, UTMessageToFPGA<instruction::omnibus_to_fpga::Data>(
+	                        instruction::omnibus_to_fpga::Data::Payload(0x2)));
+	auto message_addr_2 =
+	    std::get<UTMessageToFPGA<instruction::omnibus_to_fpga::Address>>(messages.at(2));
+	EXPECT_EQ(message_addr_2, addr_read_stop);
+}
+
+TEST(I2CTCA9554RwRegister, EncodeWrite)
+{
+	using namespace hxcomm::vx;
+
+	typename I2CTCA9554RwRegister::coordinate_type coord(
+	    halco::hicann_dls::vx::I2CTCA9554RwRegisterOnTCA9554::polarity,
+	    halco::hicann_dls::vx::TCA9554OnBoard());
+	I2CTCA9554RwRegister reg(I2CTCA9554RwRegister::Value(0x55));
+	auto messages = reg.encode_write(coord);
+
+	EXPECT_EQ(messages.size(), 4);
+	auto addr_write = UTMessageToFPGA<instruction::omnibus_to_fpga::Address>(
+	    instruction::omnibus_to_fpga::Address::Payload(i2c_tca9554_base_address));
+
+	auto addr_write_stop = UTMessageToFPGA<instruction::omnibus_to_fpga::Address>(
+	    instruction::omnibus_to_fpga::Address::Payload(
+	        i2c_tca9554_base_address | i2c_over_omnibus_stop));
+
+	auto message_addr_1 =
+	    std::get<UTMessageToFPGA<instruction::omnibus_to_fpga::Address>>(messages.at(0));
+	EXPECT_EQ(message_addr_1, addr_write);
+	auto message_data_1 =
+	    std::get<UTMessageToFPGA<instruction::omnibus_to_fpga::Data>>(messages.at(1));
+	EXPECT_EQ(
+	    message_data_1, UTMessageToFPGA<instruction::omnibus_to_fpga::Data>(
+	                        instruction::omnibus_to_fpga::Data::Payload(0x2)));
+	auto message_addr_2 =
+	    std::get<UTMessageToFPGA<instruction::omnibus_to_fpga::Address>>(messages.at(2));
+	EXPECT_EQ(message_addr_2, addr_write_stop);
+	auto message_data_2 =
+	    std::get<UTMessageToFPGA<instruction::omnibus_to_fpga::Data>>(messages.at(3));
+	EXPECT_EQ(
+	    message_data_2, UTMessageToFPGA<instruction::omnibus_to_fpga::Data>(
+	                        instruction::omnibus_to_fpga::Data::Payload(0x55)));
+}
+
+TEST(I2CTCA9554RwRegister, Ostream)
+{
+	I2CTCA9554RwRegister obj;
+	obj.set(I2CTCA9554RwRegister::Value(85));
+
+	std::stringstream stream;
+	stream << obj;
+
+	EXPECT_EQ(stream.str(), "I2CTCA9554RwRegister(0d85 0x55 0b01010101)");
+}
