6f820ccb87be2dc37bf908fc51216fbd8853bad1
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-09-09 11:57:12 +0200

    Reduce built serialization archives to JSON and PortableBinary
    Change-Id: I935e4d3e91ef886eeed20dbd4d71ccd0e246e4d4

--- include/fisch/cerealization.h
+++ include/fisch/cerealization.h
@@ -1,18 +1,12 @@
 #pragma once
 #include <cereal/cereal.hpp>
 
-#include <cereal/archives/binary.hpp>
 #include <cereal/archives/json.hpp>
 #include <cereal/archives/portable_binary.hpp>
-#include <cereal/archives/xml.hpp>
 
 #include "hate/visibility.h"
 
 #define EXPLICIT_INSTANTIATE_CEREAL_SERIALIZE(CLASS_NAME)                                          \
-	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
-	    cereal::BinaryOutputArchive&, std::uint32_t const);                                        \
-	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
-	    cereal::BinaryInputArchive&, std::uint32_t const);                                         \
 	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
 	    cereal::PortableBinaryOutputArchive&, std::uint32_t const);                                \
 	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
@@ -20,8 +14,4 @@
 	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
 	    cereal::JSONOutputArchive&, std::uint32_t const);                                          \
 	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
-	    cereal::JSONInputArchive&, std::uint32_t const);                                           \
-	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
-	    cereal::XMLOutputArchive&, std::uint32_t const);                                           \
-	template SYMBOL_VISIBLE void CLASS_NAME ::serialize(                                           \
-	    cereal::XMLInputArchive&, std::uint32_t const);
+	    cereal::JSONInputArchive&, std::uint32_t const);
