c497417975d098ab397a43e8f99f0bf6f2c76c51
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2022-01-17 10:51:43 +0100

    Adapt to c/16251
    Depends-On: 16251
    Change-Id: Id753a24d5838e0b90619917c1adcf3b0708b2f8e

--- tests/hw/fisch/vx/main.cpp
+++ tests/hw/fisch/vx/main.cpp
@@ -1,6 +1,6 @@
 #include <string>
 
-#include "hxcomm/common/ensure_local_quiggeldy.h"
+#include "hxcomm/vx/ensure_local_quiggeldy.h"
 #include <chrono>
 #include <boost/program_options.hpp>
 #include <gtest/gtest.h>
@@ -45,7 +45,7 @@ int main(int argc, char* argv[])
 #ifdef FISCH_TEST_LOCAL_QUIGGELDY
 	// ensure that a local quiggeldy is running; this is a noop if we run
 	// already in quiggeldy environment
-	[[maybe_unused]] hxcomm::EnsureLocalQuiggeldy context;
+	[[maybe_unused]] hxcomm::vx::EnsureLocalQuiggeldy context;
 	using namespace std::literals::chrono_literals;
 	std::this_thread::sleep_for(1s);
 #endif
