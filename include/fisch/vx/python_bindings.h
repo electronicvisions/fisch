b1d40d1ac0683682035cd200d12d44218358e52f
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2022-10-06 14:56:08 +0200

    Fix pyhalco dependency in pyfisch
    Change-Id: I504117565072781bb1e2c773885e1c6be263f034

--- include/fisch/vx/python_bindings.h
+++ include/fisch/vx/python_bindings.h
@@ -1,6 +1,21 @@
 #pragma once
 
-// includes needed for python bindings
+#include "fisch/vx/genpybind.h"
+
+GENPYBIND_TAG_FISCH_VX
+#if (FISCH_VX_CHIP_VERSION == 2)
+GENPYBIND_MANUAL({
+	parent.attr("__variant__") = "pybind11";
+	parent->py::module::import("pyhalco_hicann_dls_vx_v2");
+});
+#elif (FISCH_VX_CHIP_VERSION == 3)
+GENPYBIND_MANUAL({
+	parent.attr("__variant__") = "pybind11";
+	parent->py::module::import("pyhalco_hicann_dls_vx_v3");
+});
+#else
+#error "Unknown FISCH_VX_CHIP_VERSION value."
+#endif
 
 #include "fisch/vx/fisch.h"
 
