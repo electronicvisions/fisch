4dbec978828f49c1c6cddf5431a7924c69fd561c
Joscha <joscha.ilmberger@kip.uni-heidelberg.de>
2023-06-23 12:15:33 +0200

    Check chip carrier version in temperature sensor test
    Change-Id: Id078c742838b1240f2713ad108815ec95df1a3f0

--- tests/hw/fisch/vx/test-readout_i2c_temp.cpp
+++ tests/hw/fisch/vx/test-readout_i2c_temp.cpp
@@ -8,6 +8,7 @@
 #include "fisch/vx/playback_program_builder.h"
 #include "fisch/vx/run.h"
 #include "halco/hicann-dls/vx/coordinates.h"
+#include "hwdb4cpp/hwdb4cpp.h"
 #include "hxcomm/vx/connection_from_env.h"
 #include "hxcomm/vx/target.h"
 
@@ -25,6 +26,27 @@ TEST(I2CTempRegister, Readout)
 	        },
 	        connection)) {
 		GTEST_SKIP() << "Connection does not support execution on hardware.";
+	}
+
+	auto const connection_unique_identifier =
+	    std::visit([](auto const& conn) { return conn.get_unique_identifier(); }, connection);
+
+	auto const [hxcube_id, fpga_id, _, __] =
+	    hwdb4cpp::HXCubeSetupEntry::get_ids_from_unique_branch_identifier(
+	        connection_unique_identifier);
+
+	hwdb4cpp::database hwdb;
+	hwdb.load(hwdb4cpp::database::get_default_path());
+	auto& hxcube_setup_entry = hwdb.get_hxcube_setup_entry(hxcube_id);
+	if (!hxcube_setup_entry.fpgas.contains(fpga_id)) {
+		throw std::runtime_error("HXCubeSetupEntry doesn't feature FPGA ID from connection.");
+	}
+	if (!hxcube_setup_entry.fpgas[fpga_id].wing.has_value()) {
+		throw std::runtime_error("HXCubeFPGAEntry doesn't feature wing.");
+	}
+	if (hxcube_setup_entry.fpgas[fpga_id].wing.value().handwritten_chip_serial < 75) {
+		// old chip carrier version, no temperature sensor available
+		return;
 	}
 
 	PlaybackProgramBuilder builder;
