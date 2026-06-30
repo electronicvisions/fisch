603a63037bfb11550ded82b319cf7716092d562f
Robin Heinemann <robin.heinemann@kip.uni-heidelberg.de>
2026-06-30 18:48:36 +0000

    chore: remove extoll
    Change-Id: Iacf57b9303577b5dd3f441809c1ad95edaf69d45

--- include/fisch/vx/coordinates.h
+++ include/fisch/vx/coordinates.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "halco/hicann-dls/vx/barrier.h"
-#include "halco/hicann-dls/vx/extoll.h"
 #include "halco/hicann-dls/vx/fpga.h"
 #include "halco/hicann-dls/vx/i2c.h"
 #include "halco/hicann-dls/vx/jtag.h"
